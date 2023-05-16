0a7f3ce88333b28bc4cd99e7dd62cfca9a936e95
AshwinRachha <ashwin.rachha@gmail.com>
2023-05-16 11:57:49 -0400

    Initial commit

---
+++ backend/app/api.py
@@ -0,0 +1,51 @@
+import sqlite3
+from fastapi import FastAPI
+import uvicorn
+
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "localhost:3000"
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+# Create a function to retrieve the file contents based on the solution index
+def get_file_contents(index: str) -> str:
+    conn = sqlite3.connect('leetcode.db')
+    cursor = conn.cursor()
+
+    # Execute the SELECT query to retrieve the file contents for the given index
+    cursor.execute("SELECT contents FROM file_contents WHERE solution_index=?", (index,))
+    result = cursor.fetchone()
+
+    # Close the database connection
+    conn.close()
+
+    # Check if a file contents were found for the given index
+    if result is None:
+        return f"No file contents found for index {index}"
+
+    # Return the file contents as a string
+    return result[0]
+
+# Define a route to retrieve the file contents based on the solution index
+@app.get("/solution/{solutionIndex}")
+async def get_solution(index: str):
+    # Retrieve the file contents for the given index
+    contents = get_file_contents(index)
+
+    # Return the file contents as a JSON response
+    return {"contents": contents}
